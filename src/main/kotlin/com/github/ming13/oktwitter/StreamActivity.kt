14c0f4ecfe347d9871f228d90ab64d198018e288
Artur Dryomov <artur.dryomov@gmail.com>
2016-08-05 12:40:25 +0300

    Add bare state.

---
+++ src/main/kotlin/com/github/ming13/oktwitter/StreamActivity.kt
@@ -0,0 +1,11 @@
+package com.github.ming13.oktwitter
+
+import android.app.Activity
+import android.os.Bundle
+
+class StreamActivity : Activity()
+{
+    override fun onCreate(state: Bundle?) {
+        super.onCreate(state)
+    }
+}
